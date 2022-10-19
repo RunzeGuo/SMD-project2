a251020be41fac51ba5b7b3e195db4461764b2b0
RunzeGuo <grz1614185525@gmail.com>
2022-10-20 01:54:23 +1100

    put suit & rank in a class

--- src/thrones/game/GameOfThrones.java
+++ src/thrones/game/GameOfThrones.java
@@ -11,61 +11,14 @@ import java.io.FileReader;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static thrones.game.SuitAndRank.*;
+
 @SuppressWarnings("serial")
 public class GameOfThrones extends CardGame {
 
-    enum GoTSuit { CHARACTER, DEFENCE, ATTACK, MAGIC }
-    public enum Suit {
-        SPADES(GoTSuit.DEFENCE),
-        HEARTS(GoTSuit.CHARACTER),
-        DIAMONDS(GoTSuit.MAGIC),
-        CLUBS(GoTSuit.ATTACK);
-        Suit(GoTSuit gotsuit) {
-            this.gotsuit = gotsuit;
-        }
-        private final GoTSuit gotsuit;
 
-        public boolean isDefence(){ return gotsuit == GoTSuit.DEFENCE; }
 
-        public boolean isAttack(){ return gotsuit == GoTSuit.ATTACK; }
 
-        public boolean isCharacter(){ return gotsuit == GoTSuit.CHARACTER; }
-
-        public boolean isMagic(){ return gotsuit == GoTSuit.MAGIC; }
-    }
-
-    public enum Rank {
-        // Reverse order of rank importance (see rankGreater() below)
-        // Order of cards is tied to card images
-        ACE(1), KING(10), QUEEN(10), JACK(10), TEN(10), NINE(9), EIGHT(8), SEVEN(7), SIX(6), FIVE(5), FOUR(4), THREE(3), TWO(2);
-        Rank(int rankValue) {
-            this.rankValue = rankValue;
-        }
-        private final int rankValue;
-        public int getRankValue() {
-            return rankValue;
-        }
-    }
-
-    /*
-    Canonical String representations of Suit, Rank, Card, and Hand
-    */
-    String canonical(Suit s) { return s.toString().substring(0, 1); }
-
-    String canonical(Rank r) {
-        switch (r) {
-            case ACE: case KING: case QUEEN: case JACK: case TEN:
-                return r.toString().substring(0, 1);
-            default:
-                return String.valueOf(r.getRankValue());
-        }
-    }
-
-    String canonical(Card c) { return canonical((Rank) c.getRank()) + canonical((Suit) c.getSuit()); }
-
-    String canonical(Hand h) {
-        return "[" + h.getCardList().stream().map(this::canonical).collect(Collectors.joining(",")) + "]";
-    }
     static public int seed;
     static Random random;
 
@@ -153,7 +106,7 @@ public class GameOfThrones extends CardGame {
     Font bigFont = new Font("Arial", Font.BOLD, 36);
     Font smallFont = new Font("Arial", Font.PLAIN, 10);
 
-    // boolean[] humanPlayers = { true, false, false, false};
+    //boolean[] humanPlayers = { true, false, false, false};
     boolean[] humanPlayers = { false, false, false, false};
 
 
